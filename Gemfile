e9b51396eb75a97e1af9c70eb2a1f6bde7eb9eb1
puchuu <aladjev.andrew@gmail.com>
2011-06-29 10:38:03 +0300

    books

--- Gemfile
+++ Gemfile
@@ -7,6 +7,9 @@ gem "rails", "3.0.9"
 
 gem "mysql"
 gem "yaml_db"
+gem "redcarpet"
+gem "albino"
+gem "nokogiri"
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110627145552) do
+ActiveRecord::Schema.define(:version => 20110628114847) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(:version => 20110627145552) do
   create_table "tags", :force => true do |t|
     t.string   "name"
     t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.text     "login"
+    t.string   "pass"
+    t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
